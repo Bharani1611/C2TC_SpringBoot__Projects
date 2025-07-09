bfd2d0f1d32ccb5214bc9b0530937b767e536de8
Bharani <116373452+Bharani1611@users.noreply.github.com>
2025-07-09 22:08:33 +0530

    my hand on my first Springboot

---
+++ SpringBoot_Eg/src/main/java/com/tnif/Springboot_eg/SpringBootEgApplication.java
@@ -0,0 +1,14 @@
+package com.tnif.Springboot_eg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEgApplication.class, args);
+		System.out.println("Bharani_Dass");
+	}
+
+}
